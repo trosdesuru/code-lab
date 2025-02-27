f4114ea470523160ab6b65c9c94dfb4951be18bc
trosdesuru <eduardhernandezventos@gmail.com>
2025-02-27 18:38:18 +0100

    added index.js

--- algorithms/FizzBuzz.js
+++ algorithms/FizzBuzz.js
@@ -1,4 +1,11 @@
-/* FizzBuzz */
+/* File: FizzBuzz.js */
+/* Date: 09/20/2020 */
+/* Author: Eduard H. */
+/* Description: FizzBuzz Algorithm & Variables */
+
+/**********************************/
+/* ***** FizzBuzz Algorithm ***** */
+/**********************************/
 
 function fizzBuzz(n) {
     for (let i = 1; i <= n; i++) {
@@ -9,9 +16,11 @@ function fizzBuzz(n) {
     }
 }
 
-/* ***** Function Variable ***** */
+/*********************************/
+/* ***** FizzBuzz Variable ***** */
+/*********************************/
 
-function fizzBuzz(n) {
+function fizzBuzz_var01(n) {
     for (let i = 1; i < n; i++) {
         if (i % 15 === 0) console.log('FizzBuzz')
         else if (i % 3 === 0) console.log('Fizz')
@@ -20,11 +29,17 @@ function fizzBuzz(n) {
     }
 }
 
-/* ***** Function variable ***** */
+/*********************************/
+/* ***** FizzBuzz variable ***** */
+/*********************************/
 
-function ft_fizzBuzz(n) {
+function fizzBuzz_var02(n) {
     return Array.from({ length: n }, (_, i) => {
         let x = i + 1
         return (x % 15 === 0 ? 'FizzBuzz' : x % 3 === 0 ? 'Fizz' : x % 5 === 0 ? 'Buzz' : x)
     })
 }
+
+/*********************************/
+/* ***** FizzBuzz variable ***** */
+/*********************************/
